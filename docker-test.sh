ba283aeb1fc907fa0bb60c797c3b614cc3a3af56
Tan En De <ende.tan@starfivetech.com>
2023-10-28 21:05:57 +0800

    install trap handler to restore ownership on exit

--- docker-test.sh
+++ docker-test.sh
@@ -4,7 +4,24 @@ REPO_DIR=/tmp/linux-loader
 REPO_MOUNT_POINT=/workdir
 
 ## CI: build-gnu-riscv64
-docker run -it --rm -v $REPO_DIR:$REPO_MOUNT_POINT --workdir $REPO_MOUNT_POINT rustvmm/dev:v29_riscv64 sh -c -e "RUSTFLAGS=\"-D warnings\" cargo build --release --target=riscv64gc-unknown-linux-gnu --config target.riscv64gc-unknown-linux-gnu.linker=\\\"riscv64-linux-gnu-gcc\\\" && chown -R $(id -u):$(id -u) $REPO_MOUNT_POINT"
+docker run -it --rm \
+	-v $REPO_DIR:$REPO_MOUNT_POINT \
+	--workdir $REPO_MOUNT_POINT \
+	rustvmm/dev:v29_riscv64 \
+	sh -c -e \
+	"trap 'chown -R $(id -u):$(id -u) $REPO_MOUNT_POINT' 0; \
+	RUSTFLAGS=\"-D warnings\" \
+	cargo build --release --target=riscv64gc-unknown-linux-gnu \
+	--config target.riscv64gc-unknown-linux-gnu.linker=\\\"riscv64-linux-gnu-gcc\\\""
 
 ## CI: unittests-gnu-riscv64
-docker run -it --rm -v $REPO_DIR:$REPO_MOUNT_POINT --workdir $REPO_MOUNT_POINT rustvmm/dev:v29_riscv64 sh -c -e "cargo test --no-run --target=riscv64gc-unknown-linux-gnu --config target.riscv64gc-unknown-linux-gnu.linker=\\\"riscv64-linux-gnu-gcc\\\" && qemu.sh && chown -R $(id -u):$(id -u) $REPO_MOUNT_POINT"
+docker run -it --rm \
+	-v $REPO_DIR:$REPO_MOUNT_POINT \
+	--workdir $REPO_MOUNT_POINT \
+	rustvmm/dev:v29_riscv64 \
+	sh -c -e \
+	"trap 'chown -R $(id -u):$(id -u) $REPO_MOUNT_POINT' 0; \
+	cargo test --no-run \
+	--target=riscv64gc-unknown-linux-gnu \
+	--config target.riscv64gc-unknown-linux-gnu.linker=\\\"riscv64-linux-gnu-gcc\\\" \
+	&& qemu.sh"
